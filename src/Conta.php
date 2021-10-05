210eecbb5084337dacbd78a29eff8fb93f321c00
Leandro Ferreira Marcelli <suporte@integracaosistema.com.br>
2021-10-05 13:53:12 -0300

    Implementando o método sacar

--- src/Conta.php
+++ src/Conta.php
@@ -1,7 +1,16 @@
 <?php
 
-class Conta{
+class Conta
+{
     public string $cpfTitular;
     public string $nomeTitular;
     public float $saldo;
+
+    public function sacar(float $valorSacar){
+        if($valorSacar > $this->saldo){
+            echo 'Saldo indisponível';
+        } else {
+            $this->sqldo -= $valorSacar;
+        }
+    }
 }
